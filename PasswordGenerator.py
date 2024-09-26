e8b87b4ffc136a656fac298a2fdf3a3d09881ac3
IL-Studesnt-012 <spaiphyo1997@gmail.com>
2024-09-26 20:24:16 +0630

    changing from GUI to Console

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,19 +1,13 @@
 import random
 import string
 import re
-from tkinter import *
-
 i = 0
 generatedpass = ""
 
-def generate():
-    length = mylabelBox.get()
+def generate(length):
     generatedpass = string.ascii_letters+ string.digits+ string.punctuation
     userPassword=''.join(random.choice(generatedpass) for x in range(int(length)))
-    # myLabel = Label(root,text=str(userPassword))
-    # myLabel.pack()
-    mylabelBox2 = Entry(root,textvariable=str(userPassword))
-    mylabelBox2.pack()
+    return userPassword
 
 
 AsciiPattern = '[a-zA-Z]'
@@ -28,9 +22,8 @@ def encrypt(words):
         else:
             convert = ord(x)+4
             converttoword = converttoword+ chr(convert)
-    # return converttoword
-    myLabel = Label(root,text=str(converttoword))
-    myLabel.pack()
+    return converttoword
+
 
 def decrypt(words):
     convert = 0
@@ -44,34 +37,17 @@ def decrypt(words):
             converttoword = converttoword+ chr(convert)
     return converttoword
 
-root = Tk()
-myLabel = Label(root,text="Please Enter choose Length of Password:  ")
-myLabel.pack()
-mylabelBox = Entry(root)
-mylabelBox.pack()
-myButton = Button(root,text="Generate",command=generate)
-myButton.pack()
-myButton1 = Button(root,text="Encrypt",command=encrypt)
-myButton1.pack()
-myButton2 = Button(root,text="Decrypt",command=decrypt)
-mylabelBox2 = Entry(root)
-mylabelBox2.pack()
 
-# userPreferNumber = int(input("Please Enter choose Length of password : "))
+userPreferNumber = int(input("Please Enter choose Length of password : "))
+
+userPass = generate(userPreferNumber)
+print("Password for user :",userPass)
+
+encryptedPass = encrypt(userPass)
+print("Encryped Password for user:",encryptedPass)
+
+decryptedPass = decrypt(encryptedPass)
+
+print("Decryped Password for user:",decryptedPass)
 
 
-
-
-
-# userPass = generate(userPreferNumber)
-# print(userPass)
-
-# encryptedPass = encrypt(userPass)
-# print(encryptedPass)
-
-# decryptedPass = decrypt(encryptedPass)
-
-# print(decryptedPass)
-
-
-root.mainloop()
